3e2b3e0059db908896c12b8e9d5139359b95df3f
Manuel Ceron <ceronman@gmail.com>
2023-08-14 21:55:29 +0200

    More pest expermients.

--- pestproto/src/main.rs
+++ pestproto/src/main.rs
@@ -12,26 +12,29 @@ fn main() {
             234
             435
     ")
-        .expect("parse error")
-        .next()
-        .unwrap();
+        .expect("parse error");
 
-    for literal in program.into_inner() {
-        match literal.as_rule() {
-            Rule::int_literal => {
-                for kind in literal.into_inner() {
-                    match kind.as_rule() {
-                        Rule::dec_literal => println!("Found dec: {}", kind.as_str()),
-                        Rule::hex_literal => println!("Found hex: {}", kind.as_str()),
-                        Rule::oct_literal => println!("Found dec: {}", kind.as_str()),
-                        Rule::bin_literal => println!("Found bin: {}", kind.as_str()),
-                        _ => println!("Found something else {:?}", kind.as_rule())
-                    }
-                }
-
-            },
-            Rule::EOI => (),
-            _ => println!("Found something else {:?}", literal.as_rule())
-        }
+    for token in program.tokens() {
+        println!(">>>>{:?}", token)
     }
+
+
+    // for literal in program.into_inner() {
+    //     match literal.as_rule() {
+    //         Rule::int_literal => {
+    //             for kind in literal.into_inner() {
+    //                 match kind.as_rule() {
+    //                     Rule::dec_literal => println!("Found dec: {}", kind.as_str()),
+    //                     Rule::hex_literal => println!("Found hex: {}", kind.as_str()),
+    //                     Rule::oct_literal => println!("Found dec: {}", kind.as_str()),
+    //                     Rule::bin_literal => println!("Found bin: {}", kind.as_str()),
+    //                     _ => println!("Found something else {:?}", kind.as_rule())
+    //                 }
+    //             }
+    //
+    //         },
+    //         Rule::EOI => (),
+    //         _ => println!("Found something else {:?}", literal.as_rule())
+    //     }
+    // }
 }
