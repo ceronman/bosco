8b5ccf417f8b1649c47fa431b5368b626645620e
Manuel Ceron <ceronman@gmail.com>
2024-05-10 21:21:39 +0200

    Test emmiting wasm using wasm-encoder dependency.

--- src/wat/test.rs
+++ src/wat/test.rs
@@ -51,3 +51,47 @@ fn hello_world() {
     };
     println!("{}", hello_world.to_wat());
 }
+
+#[test]
+fn hello_world_encoder() {
+    use wasm_encoder::*;
+
+    let mut module = Module::new();
+    let mut imports = ImportSection::new();
+    let mut types = TypeSection::new();
+    types.function(vec![ValType::I32, ValType::I32], vec![]);
+    types.function(vec![], vec![]);
+    imports.import("js", "print", EntityType::Function(0));
+    imports.import("js", "mem", EntityType::Memory(MemoryType {
+        minimum: 0,
+        maximum: None,
+        memory64: false,
+        shared: false,
+        page_size_log2: None,
+    }));
+    let mut data = DataSection::new();
+    data.active(0, &ConstExpr::i32_const(0), "Hello from Wasm Encoder!".bytes());
+
+    let mut functions = FunctionSection::new();
+    functions.function(1);
+
+    let mut codes = CodeSection::new();
+    let mut function = Function::new(vec![]);
+    function.instruction(&Instruction::I32Const(0));
+    function.instruction(&Instruction::I32Const(24));
+    function.instruction(&Instruction::Call(0));
+    function.instruction(&Instruction::End);
+    codes.function(&function);
+
+    module.section(&types);
+    module.section(&imports);
+    module.section(&data);
+    module.section(&functions);
+    module.section(&codes);
+    module.section(ExportSection::new().export("hello", ExportKind::Func, 1));
+
+    let bytes = module.finish();
+
+    let wat = wasmprinter::print_bytes(bytes).unwrap();
+    println!("{}", wat);
+}
--- web/hello.wat
+++ web/hello.wat
@@ -1,8 +1,13 @@
 (module
-    (import "js" "print" (func $print (param $start i32) (param $len i32)))
-    (import "js" "mem" (memory 1))
-    (data (i32.const 0) "Hello from Wasm!")
-    (func $hello (export "hello")
-        (i32.const 0)
-        (i32.const 17)
-        (call $print)))
+  (type (;0;) (func (param i32 i32)))
+  (type (;1;) (func))
+  (import "js" "print" (func (;0;) (type 0)))
+  (import "js" "mem" (memory (;0;) 0))
+  (data (;0;) (i32.const 0) "Hello from Wasm Encoder!")
+  (func (;1;) (type 1)
+    i32.const 0
+    i32.const 24
+    call 0
+  )
+  (export "hello" (func 1))
+)
